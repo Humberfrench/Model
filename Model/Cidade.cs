8c26e8c62fd60eb26e8e0914c644cc40f7faaed6
Humberto Almeida <humberto.almeida@french.com.br>
2015-09-11 17:24:43 -0300

    cidade e estado

---
+++ Model/Cidade.cs
@@ -0,0 +1,13 @@
+﻿namespace Models
+{
+    class Cidade 
+    {
+        public int Codigo{ get; set; }
+        public string Nome { get; set; }
+        public override string ToString()
+        {
+            return Nome.ToString();
+        }
+        public Estado UF { get; set; }
+    }
+}
